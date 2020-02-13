780bc2abad4ce66879f4ce152594d9dc0e07c947
mdespe <mikespe23@gmail.com>
2020-02-13 12:33:19 -0600

    step 2 done

--- app/src/main/java/com/example/lab3milestone2/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone2/MainActivity.java
@@ -4,6 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.app.Fragment;
+
+import android.app.FragmentManager;
+
+import android.app.FragmentTransaction;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.view.View;
+
+import android.widget.Button;
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
